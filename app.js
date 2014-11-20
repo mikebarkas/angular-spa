489970f5104e2c9fc8c58dfb0372c281781e44ab
Mike Barkas <mike@barkas.com>
2014-11-19 21:47:25 -0500

    Config router function.

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'inc/one.html',
+      controller: 'mainController'
+    })
+    .when('/two', {
+      templateUrl: 'inc/two.html',
+      controller: 'mainController'
+    })
+});
+
 
 myApp.controller('mainController', ['$scope', '$location',
   function($scope, $location) {
